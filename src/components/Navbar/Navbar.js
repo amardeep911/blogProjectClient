d44830263e102f88fe32915856d24b1663cd0ddb
ranjan_kuldeep <95350799+ranjankuldeep@users.noreply.github.com>
2023-01-30 10:07:08 +0530

    monday morning upload

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,38 +1,59 @@
-import React from 'react'
-import styles from './navbar.module.css'
-import { useState } from 'react';
+import React from "react";
 import { useNavigate } from "react-router-dom";
-const Navbar = () => {
-  const [isClosed, setIsClosed] = useState(true);
+import { slide as Menu } from "react-burger-menu";
+import style from "./styles.css";
+import styles from "./navbar.module.css";
 
-  function showHandler(){
-    setIsClosed(!true)
-  }
+const Navbar = () => {
   const navigate = useNavigate();
-   function submitHandler(){
-    navigate("/editor")
+  function submitHandler() {
+    navigate("/editor");
+  }
+  function showSettings(event) {
+    event.preventDefault();
   }
   return (
-    <div className={styles.container}>
-      <div className={styles.logo}>
-        BlogsKart
+    <>
+      <Menu styles={style} className={"my-menu"} right={true}>
+        <a id="home" className="menu-item" href="/">
+          Home
+        </a>
+        <a id="about" className="menu-item" href="/about">
+          About
+        </a>
+        <a id="create" className="menu-item" href="/ff">
+          Contact
+        </a>
+        <a onClick={showSettings} className="menu-item--small" href="/">
+          Settings
+        </a>
+      </Menu>
+      <div className={styles.container}>
+        <div className={styles.logo}>BlogsKart</div>
+        <div className={styles.items}>
+          <ul className={styles.list}>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/">My Blogs</a>
+            </li>
+            <li>
+              <a href="/" onClick={submitHandler}>
+                Write Blog
+              </a>
+            </li>
+            <li>
+              <a href="/">About me</a>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.btn}>
+          <button className={styles.navbarButton}>LogOut</button>
+        </div>
       </div>
-      <div className={styles.items}>
-        
-        <ul className={styles.list}>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">My Blogs</a></li>
-          <li><a href="#" onClick={submitHandler}>Write Blog</a></li>
-          <li><a href="#">About me</a></li>
-        </ul>
-      </div>
-      <div className={styles.btn}>
-        <button className={styles.navbarButton}>LogOut</button>
-        
-      </div>
-      
-    </div>
-  )
-}
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
